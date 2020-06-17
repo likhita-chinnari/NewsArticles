1ed6b5c58a8b5eca516fa17a88c8f5547b8a9fc0
likhita-chinnari <47099864+likhita-chinnari@users.noreply.github.com>
2020-06-17 14:35:57 +0530

    Add files via upload
    added the project files

---
+++ rest_config.js
@@ -0,0 +1,4 @@
+export const articles_url = 'http://newsapi.org/v2/top-headlines';
+export const country_code = 'in';
+export const category = 'general';
+export const _api_key = 'f2cc2bbaef144b8b8340b5abd6fbed6d';
